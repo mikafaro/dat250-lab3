54b67337d53daf15897cad48539f4f77b2478b49
Mikael Færøvik <mikael.farovik@gmail.com>
2024-09-19 21:36:28 +0200

    header, footer and tabs

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,31 @@
 <script>
+    import Footer from "./components/Footer.svelte";
+    import Header from "./components/Header.svelte";
+    import Tabs from "./shared/Tabs.svelte";
+
+    // tabs
+    let items = ['Active Polls', 'New Poll'];
+    let activeItem = 'Active Polls';
+
+    const tabChange = (e) => {
+      activeItem = e.detail;
+    }
 </script>
 
+<Header />
 <main>
+  <Tabs {activeItem} {items} on:tabChange={tabChange}/>
+  {#if activeItem === 'Active Polls'}
+    <p>Poll list component here</p>
+  {:else if activeItem === 'New Poll'}
+    <p>New poll component here</p>
+  {/if}
 </main>
+<Footer />
 
 <style>
+  main{
+    max-width: 960px;
+    margin: 40px auto;
+  }
 </style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,21 @@
+<script>
+
+</script>
+
+<header>
+    <h1>
+        Mikael's Poll App
+    </h1>
+</header>
+
+<style>
+    header{
+        background: #44624a;
+        padding: 20px;
+    }
+    h1{
+        margin: 0;
+        text-align: center;
+        color: #f1ebe1;
+    }
+</style>
